a9404ba437cdd3e25f78d49bf0220059358b1d17
DiShinHub <dinooo.shin@gmail.com>
2022-01-02 23:23:33 +0900

    샘플 개선 및 추가

--- python_basic/chapter2/sample/ao.py
+++ python_basic/chapter2/sample/ao.py
@@ -7,7 +7,6 @@ print(type(x))
 x = 1
 y = 2
 
-
 # 더하기
 sum_xy = x + y
 print(sum_xy)
@@ -23,6 +22,17 @@ print(mul_xy)
 # 나누기
 div_xy = x/y
 print(div_xy)
+
+# x에서 y를 나눈 몫을 반환
+q_div_xy =  x // y
+print(q_div_xy)
+
+# x에서 y를 나눈 나머지를 반환
+r_div_xy =   x % y
+print(r_div_xy)
+
+# 부호를 바꿈
+print(-x)
 
 # 실수형 변수 선언
 x = 1.1
--- python_basic/chapter2/sample/so.py
+++ python_basic/chapter2/sample/so.py
@@ -1,19 +1,21 @@
 # 문자열 선언
 a = "hello, world!"
 print(a)
+print(type(a))
 
 # 문자열 인덱싱
 print(a[4])
+
+# 애러 유도
+#print(a[111])
+
+# 문자열 역 인덱싱
+print(a[-1])
 
 # 문자열 슬라이싱
 print(a[3:])
 print(a[:4])
 print(a[2:7])
-
-
-# 애러 유도
-print(a[111])
-
 
 
 a = "안녕"

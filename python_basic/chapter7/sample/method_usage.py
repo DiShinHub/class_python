07313fd96515b0617ff83f737446c23a2f810d91
DiShinHub <dinooo.shin@gmail.com>
2022-01-21 17:55:12 +0900

    7강 샘플 변경

--- python_basic/chapter7/sample/method_usage.py
+++ python_basic/chapter7/sample/method_usage.py
@@ -8,7 +8,7 @@ def minus_num(a,b):
     result = a - b
     return result
 
-def mult_num(a,b):
+def multi_num(a,b):
     result = a * b
     return result
 
@@ -30,52 +30,7 @@ def remove_extension(file_name):
 
     return file_name
 
-def add_sec(target_time, sec):
 
-    if abs(sec) >= 60:
-        print("sec 값 초과")
-        return False
-
-    hh = int(target_time[0:2])
-    mm = int(target_time[3:5])
-    ss = int(target_time[6:8])
-
-    ss += sec
-    if sec > 0:
-        
-        if ss >= 60:
-            ss = ss - 60
-            mm += 1
-        
-        if mm >= 60:
-            mm = mm - 60
-            hh += 1
-        
-        if hh >= 24:
-            hh = hh - 24
-    
-    else:
-
-        if ss < 0:
-            ss = abs(ss + 60)
-            mm += -1
-            
-        if mm < 0:
-            mm = abs(mm + 60)
-            hh += -1
-        
-        if hh < 0:
-            hh = abs(hh + 24)
-    
-    hh = str(hh).zfill(2)
-    mm = str(mm).zfill(2)
-    ss = str(ss).zfill(2)
-    result_time = f"{hh}:{mm}:{ss}"
-    return result_time
-
-# 
-target_time = "23:59:01"
-sec = 59
-
-result_time = add_sec(target_time, sec)
-print(result_time)
+file_name = "test.png"
+new_file_name = remove_extension(file_name)
+print(new_file_name)
